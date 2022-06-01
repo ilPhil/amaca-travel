091b17db597a63861e1596a6c92479e9abd42f4e
Alessandro <Alexanderg90@hotmail.it>
2022-06-01 17:26:50 +0200

    style fix

--- components/Map.js
+++ components/Map.js
@@ -5,9 +5,8 @@ function MapBox() {
     return <Map
         mapboxAccessToken={process.env.mapbox_key}
         mapStyle="mapbox://styles/mrcalavera/cl3u94ry8002b15qg5txm1t9y/draft"
+        style={{width: 600, height: 400,}} // set map wdt hgt here
         initialViewState={{
-            width: "50vw",
-            height: "50vh",
             longitude: -100,
             latitude: 40,
             zoom: 3
