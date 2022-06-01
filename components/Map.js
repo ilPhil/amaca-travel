0470a6accb2a53be703c7b61e852581a47da2291
Alessandro <Alexanderg90@hotmail.it>
2022-06-01 17:22:45 +0200

    set width&height & fix

--- components/Map.js
+++ components/Map.js
@@ -1,22 +1,18 @@
-import mapboxgl from "mapbox-gl";
-import { GeolocateControl, Map } from "react-map-gl";
+
+import { Map } from "react-map-gl";
 
 function MapBox() {
-    const map = new mapboxgl.Map({
-        container: "map", // container ID
-        center: [7.57, 41.893, 12.499], //starting pos.
-        zoom: 13, // starting zoom
-    })
     return <Map
         mapboxAccessToken={process.env.mapbox_key}
         mapStyle="mapbox://styles/mrcalavera/cl3u94ry8002b15qg5txm1t9y/draft"
         initialViewState={{
+            width: "50vw",
+            height: "50vh",
             longitude: -100,
             latitude: 40,
             zoom: 3
         }}
     >
-        <GeolocateControl />
     </Map>
 }
 
