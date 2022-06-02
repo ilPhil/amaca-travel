19929eea19f34c4d2163c36594c9072ccd0f788e
Christian <chrise79456@gmail.com>
2022-06-02 15:47:50 +0200

    Commit add layout arrange elements

--- components/Map.js
+++ components/Map.js
@@ -5,7 +5,7 @@ function MapBox() {
     return <Map
         mapboxAccessToken={process.env.mapbox_key}
         mapStyle="mapbox://styles/mrcalavera/cl3u94ry8002b15qg5txm1t9y/draft"
-        style={{width: 600, height: 400,}} // set map wdt hgt here
+        style={{width: 1900, height: 600,}} // set map wdt hgt here
         initialViewState={{
             longitude: -100,
             latitude: 40,
