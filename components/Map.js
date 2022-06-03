997fef02c9f76cf121948f6afdc6a60500b443c4
Alessandro <Alexanderg90@hotmail.it>
2022-06-03 19:20:16 +0200

    added cities markers

--- components/Map.js
+++ components/Map.js
@@ -1,24 +1,43 @@
-// import CardItem from "./CardItem/CardItem";
-import { Map, GeolocateControl, Marker } from "react-map-gl";
+// import MapCard from "./MapCard/MapCard";
+import * as React from "react";
+import { Map, GeolocateControl, Marker, Popup } from "react-map-gl";
+import { getCities } from "../utils/fetch";
 
 function MapBox() {
+  const [showPopup, setShowPopup] = React.useState(true);
+  // const data = getCities();
+  const [coordinates, setCoordinates] = React.useState([]);
+  // const { latitude, longitude } = data;
+  // const latitude = data.latitude;
+  // const longitude = data.longitude;
+  React.useEffect(() => {
+    getCities().then((data) => setCoordinates(data));
+  }, []);
+
   return (
     <Map
       mapboxAccessToken={process.env.mapbox_key}
       mapStyle="mapbox://styles/mrcalavera/cl3u94ry8002b15qg5txm1t9y/draft"
-      style={{ width: 1900, height: 600 }} // set map wdt hgt here
+      style={{ width: 600, height: 400 }} // set map wdt hgt here
       initialViewState={{
         longitude: -100,
         latitude: 40,
-        zoom: 3,
+        zoom: 4,
       }}
     >
-      <Marker
-        style={{ width: 100, height: 20 }}
-        longitude={-100}
-        latitude={40}
-        anchor="bottom"
-      ></Marker>
+      {coordinates &&
+        coordinates.map((city) => (
+          <Marker
+            key={city.id}
+            longitude={city.longitude}
+            latitude={city.latitude}
+            anchor={"top"}
+          ></Marker>
+        ))}
+      {/* {showPopup && (
+            <Popup longitude={-100} latitude={40} anchor="top" onClose={() => setShowPopup(false)}>Gran festa</Popup>
+        )} */}
+
       <GeolocateControl />
     </Map>
   );
