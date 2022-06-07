30c77b3d770401c604cc74b8d730bd85e93c8112
Bluealastor <chrise79456@gmail.com>
2022-06-08 00:13:27 +0200

    fix/29-bug keys

--- components/CardItem/CardItem.jsx
+++ components/CardItem/CardItem.jsx
@@ -1,37 +1,35 @@
 import style from "./index.module.scss";
 import { getActivities } from "../../utils/fetch";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
-export default function CardItem(title, description) {
+export default function CardItem() {
   const [activity, setActivity] = useState([]);
-  //const activities = getActivities().data
   useEffect(() => {
     getActivities().then((act) => setActivity(act));
   }, []);
 
   const data = activity.data;
 
-  const formatContentText = (content) => {
-    const textArr = content && content.split(" ");
-    return content && textArr.splice(1, textArr.length - 2).join(" ");
-  };
+ 
 
   return (
     <div className={style.cardsWrapper}>
-      {/* {console.log(data)} */}
-      {data &&
-        data.map((actList) => (
-          <div className={style.card} key={actList.uuid}>
+      {data?.map(({uuid, cover_image_url, title,about}) => (
+          <Link href={`activity/${uuid}`} key={uuid}>
+
+          <div className={style.card} >
             <div>
-              <img className={style.poster} src={actList.cover_image_url} />
+              <img className={style.poster} src={cover_image_url} />
               <div className={style.container}>
-                <h1 className={style.title}>{actList.title}</h1>
+                <h4 className={style.title}>{title.substring(0,20)}</h4>
                 <p className={style.description}>
-                  {formatContentText(actList.about)}
+                  {about.substring(0, 50)}
                 </p>
               </div>
             </div>
           </div>
+          </Link>
         ))}
     </div>
   );
