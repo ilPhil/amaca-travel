b6dea9d1d82fe615491299ea4ac0d3769ef12d26
Stefano Russo <stefano.russo@confidence.com>
2022-06-08 17:15:55 +0200

    fix: crossorigin not useful

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap"
           rel="stylesheet"
